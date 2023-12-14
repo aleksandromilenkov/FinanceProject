ffd4f01f7c07dbd2436358394f316e71bf22ec15
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2023-12-14 17:12:28 +0100

    Creating the Company Page and fetching the company

--- frontend/src/Pages/CompanyPage/CompanyPage.tsx
+++ frontend/src/Pages/CompanyPage/CompanyPage.tsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { CompanyProfile } from "../../company";
+import { getCompanyProfile } from "../../api";
+import { useParams } from "react-router";
 
 interface Props {}
 
 const CompanyPage = (props: Props) => {
-  return <div>CompanyPage</div>;
+  const [company, setCompany] = useState<CompanyProfile>();
+  const { ticker } = useParams();
+  useEffect(() => {
+    const getProfileInit = async () => {
+      const result = await getCompanyProfile(ticker!);
+      setCompany(result?.data[0]);
+    };
+    getProfileInit();
+  }, [ticker]);
+  return (
+    <>
+      {company ? (
+        <div>{company.companyName}</div>
+      ) : (
+        <div>Company not found</div>
+      )}
+    </>
+  );
 };
 
 export default CompanyPage;
