139c9d410c4ab182c9f2ffee7b979725a4cd0359
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2024-01-01 17:28:21 +0100

    Installing Identity package and creating AppUser model and adding Migrations to the Database

--- api/Data/ApplicationDbContext.cs
+++ api/Data/ApplicationDbContext.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace api.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
 
         }
-        public DbSet<Stock> Stocks {get; set;}
-        public DbSet<Comment> Comments {get;set;}
-        
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
     }
 }
