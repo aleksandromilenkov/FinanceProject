e3338fc384cd1e4f1b1a6e77e38dae7c2eadd004
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2024-01-30 20:56:19 +0100

    Adding Stock to Portfolio

--- api/Controllers/PortfolioController.cs
+++ api/Controllers/PortfolioController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Extensions;
+using api.Helpers;
 using api.Interfaces;
 using api.Models;
 using Microsoft.AspNetCore.Authorization;
@@ -26,7 +27,7 @@ namespace api.Controllers
             _portfolioRepository = portfolioRepository;
         }
 
-        [HttpGet]
+        [HttpGet(Name = "GetUserPortfolio")]
         [Authorize]
         public async Task<IActionResult> GetUserPortfolio()
         {
@@ -34,6 +35,38 @@ namespace api.Controllers
             var appUser = await _userManager.FindByNameAsync(username);
             var userPortfolio = await _portfolioRepository.GetUserPortfolio(appUser);
             return Ok(userPortfolio);
+        }
+
+        [HttpPost("{symbol}")]
+        [Authorize]
+        public async Task<IActionResult> CrteateUserPortfolio([FromRoute] string symbol)
+        {
+            var username = User.GetUsername();
+            var appUser = await _userManager.FindByNameAsync(username);
+            var stock = await _stockRepository.GetStocks(new QueryObject { Symbol = symbol });
+            if (stock == null)
+            {
+                return BadRequest();
+            }
+            var userPortfolio = await _portfolioRepository.GetUserPortfolio(appUser);
+            if (userPortfolio.Any(u => u.Symbol.ToLower() == symbol.ToLower()))
+            {
+                return BadRequest("You already have this stock in your portfolio.");
+            }
+            var portfolio = new Portfolio
+            {
+                StockId = stock.FirstOrDefault().Id,
+                AppUserId = appUser.Id,
+            };
+            var portfolioCreated = await _portfolioRepository.CreatePortfolio(portfolio);
+            if (portfolioCreated == null)
+            {
+                return StatusCode(500, "Could not create portfolio.");
+            }
+            else
+            {
+                return CreatedAtRoute("GetUserPortfolio", null);
+            }
         }
 
     }
--- api/Repository/PortfolioRepository.cs
+++ api/Repository/PortfolioRepository.cs
@@ -18,6 +18,13 @@ namespace api.Repository
             _context = context;
         }
 
+        public async Task<Portfolio> CreatePortfolio(Portfolio portfolio)
+        {
+            await _context.Portfolios.AddAsync(portfolio);
+            await _context.SaveChangesAsync();
+            return portfolio;
+        }
+
         public async Task<List<Stock>> GetUserPortfolio(AppUser user)
         {
             return await _context.Portfolios.Where(p => p.AppUserId == user.Id).Select(p => p.Stock).ToListAsync();
